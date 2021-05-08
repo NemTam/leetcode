38781a967bde282677f0538b32a7e4a164f83811
Tamas Nemeth <tamas.nemeth@bvtech.io>
2021-05-08 14:21:19 +0200

    number of islands

---
+++ python/medium/200_number_of_islands.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+def numIslands(grid: List[List[str]]) -> int:
+    count = 0
+    if not grid:
+        return 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == "1":
+                count += 1
+            dfs(grid, i, j)
+
+    return count
+
+
+def dfs(grid, i, j):
+    if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != "1":
+        return
+    grid[i][j] = 'X'
+
+    dfs(grid, i - 1, j)
+    dfs(grid, i + 1, j)
+    dfs(grid, i, j + 1)
+    dfs(grid, i, j - 1)
+
+
+grid = [
+    ["1", "1", "0", "0", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "1", "0", "0"],
+    ["0", "0", "0", "1", "1"]
+]
+
+res = numIslands(grid)
+print(res)
